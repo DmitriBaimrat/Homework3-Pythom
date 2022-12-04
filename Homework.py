3c82323eb7cb8bf34e6e223419c66a86636fdd57
Dmitriy <baimratus@mail.ru>
2022-12-04 18:58:33 +0500

    Ex4

--- Homework.py
+++ Homework.py
@@ -38,20 +38,33 @@
 # Пример:
 # - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
 
-from random import uniform
-num = int(input('Введите размер списка: '))
-list = []
-for i in range(num):
-    fl = uniform(0, 99)
-    list.append(round(fl, 2))
-min = list[0]
-max = 0
-for i in range(len(list)):
-    if max < list[i]:
-        max = list[i]
-    if min > list[i]:
-        min = list[i]
-dif = (max - int(max)) - (min - int(min))
-print(list)
-print(max, min)
-print(round(dif,2))
+# from random import uniform
+# num = int(input('Введите размер списка: '))
+# list = []
+# for i in range(num):
+#     fl = uniform(0, 99)
+#     list.append(round(fl, 2))
+# min = list[0]
+# max = 0
+# for i in range(len(list)):
+#     if max < list[i]:
+#         max = list[i]
+#     if min > list[i]:
+#         min = list[i]
+# dif = (max - int(max)) - (min - int(min))
+# print(list)
+# print(max, min)
+# print(round(dif,2))
+
+# Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+# Пример:
+# - 45 -> 101101
+# - 3 -> 11
+# - 2 -> 10
+
+s = ""
+n = int(input("Введите число для преобразовывания: "))
+while n != 0:
+    s = str(n % 2) + s
+    n //= 2
+print(s)
