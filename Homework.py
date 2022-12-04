a064e3867c2a58506b28104c6e5e40b8a27e89f8
Dmitriy <baimratus@mail.ru>
2022-12-04 18:54:02 +0500

    Ex3

--- Homework.py
+++ Homework.py
@@ -18,17 +18,40 @@
 # - [2, 3, 4, 5, 6] => [12, 15, 16];
 # - [2, 3, 5, 6] => [12, 15]
 
-from random import randint
-number = int(input('Введите размер списка: '))
+# from random import randint
+# number = int(input('Введите размер списка: '))
+# list = []
+# list2 = []
+# for i in range(number):
+#     list.append(randint(0, 9))
+# for i in range(len(list)):
+#     while i < len(list)/2 and number > len(list)/2:
+#         number = number - 1
+#         a = list[i] * list[number]
+#         list2.append(a)
+#         i += 1
+# print(list)
+# print(list2)
+
+# Задайте список из вещественных чисел. Напишите программу, которая найдёт разницу между максимальным и минимальным
+# значением дробной части элементов.
+# Пример:
+# - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
+
+from random import uniform
+num = int(input('Введите размер списка: '))
 list = []
-list2 = []
-for i in range(number):
-    list.append(randint(0, 9))
+for i in range(num):
+    fl = uniform(0, 99)
+    list.append(round(fl, 2))
+min = list[0]
+max = 0
 for i in range(len(list)):
-    while i < len(list)/2 and number > len(list)/2:
-        number = number - 1
-        a = list[i] * list[number]
-        list2.append(a)
-        i += 1
+    if max < list[i]:
+        max = list[i]
+    if min > list[i]:
+        min = list[i]
+dif = (max - int(max)) - (min - int(min))
 print(list)
-print(list2)
+print(max, min)
+print(round(dif,2))
